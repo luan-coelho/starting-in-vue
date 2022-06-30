4cd10e192f39a7d744179b5ed8476f6c55b422ec
Luan Coêlho <luancoelho.dev@gmail.com>
2022-06-30 12:12:13 -0300

    Criação de formulário de Usuário

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <DataTable />
+  <FormUser />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import DataTable from '@/components/DataTable.vue';
+import FormUser from '@/components/FormUser.vue';
 
 export default defineComponent({
   name: 'App',
-  components: { DataTable }
+  components: { FormUser }
 });
 </script>
 
